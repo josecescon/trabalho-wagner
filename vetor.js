eb5b79604ad17747df6fb54972b179a9a8214019
joaoCerqueira2010 <joao.cerqueira.nascimento@escola.pr.gov.br>
2025-08-14 08:30:53 -0300

    Add files via upload

---
+++ vetor.js
@@ -0,0 +1,35 @@
+const lanches =[
+
+ { id: "lanche1", nome: "daffara picante", preco: "32,50"},
+ { id: "lanche2", nome: "vegan daffara", preco: "35,00"},
+ { id: "lanche3", nome: "daffara clasic", preco: "20,00"},   
+ { id: "lanche4", nome: "daffarinha kids", preco: "28,90"},
+ { id: "lanche5", nome: "daffara duplo", preco: "40,00"},
+ { id: "lanche6", nome: "daffara wraps", preco: "32,00"},
+ { id: "lanche7", nome: "batatas do chef", preco: "12,90"},
+ { id: "lanche8", nome: "batata com cheddar", preco: "18,50"},
+
+
+
+]
+
+const relatorio =[
+
+
+{ id: "relatorio1", entregador: "jose", local :"jorge schimmelpfeng"},
+{ id: "relatorio2", entregador: "alex", local :"morumbi"},
+{ id: "relatorio3", entregador: "hytalo", local :"ayrton senna"},
+{ id: "relatorio4", entregador: "felipe", local :"felipe wandscheer"},
+{ id: "relatorio5", entregador: "alexandre", local :"av. parana"},
+{ id: "relatorio6", entregador: "jair", local :"av. brasil"},
+{ id: "relatorio7", entregador: "luiz", local :"rua urupes"},
+{ id: "relatorio8", entregador:  "dilma" , local :"conj. Libra"},
+
+
+
+
+
+
+
+
+]
