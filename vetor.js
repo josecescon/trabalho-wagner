c35e6af83b3d54fcb9fc705dcb0f9ac6a1689dce
josecescon <jose.cescon@escola.pr.gov.br>
2025-08-14 11:44:32 -0300

    Add files via upload

--- vetor.js
+++ vetor.js
@@ -1,35 +1,46 @@
 const lanches =[
 
- { id: "lanche1", nome: "daffara picante", preco: "32,50"},
- { id: "lanche2", nome: "vegan daffara", preco: "35,00"},
- { id: "lanche3", nome: "daffara clasic", preco: "20,00"},   
- { id: "lanche4", nome: "daffarinha kids", preco: "28,90"},
- { id: "lanche5", nome: "daffara duplo", preco: "40,00"},
- { id: "lanche6", nome: "daffara wraps", preco: "32,00"},
- { id: "lanche7", nome: "batatas do chef", preco: "12,90"},
- { id: "lanche8", nome: "batata com cheddar", preco: "18,50"},
-
-
-
+    { id: "lanche1", nome: "daffara picante", preco: "32,50"},
+    { id: "lanche2", nome: "vegan daffara", preco: "35,00"},
+    { id: "lanche3", nome: "daffara clasic", preco: "20,00"},   
+    { id: "lanche4", nome: "daffarinha kids", preco: "28,90"},
+    { id: "lanche5", nome: "daffara duplo", preco: "40,00"},
+    { id: "lanche6", nome: "daffara wraps", preco: "32,00"},
 ]
 
 const relatorio =[
 
+    { id: "relatorio1", entregador: "jose", local :"jorge schimmelpfeng"},
+    { id: "relatorio2", entregador: "alex", local :"morumbi"},
+    { id: "relatorio3", entregador: "hytalo", local :"ayrton senna"},
+    { id: "relatorio4", entregador: "felipe", local :"felipe wandscheer"},
+    { id: "relatorio5", entregador: "alexandre", local :"av. parana"},
+    { id: "relatorio6", entregador: "jair", local :"av. brasil"},
+    { id: "relatorio7", entregador: "luiz", local :"rua urupes"},
+    { id: "relatorio8", entregador:  "dilma" , local :"conj. Libra"},
+]
 
-{ id: "relatorio1", entregador: "jose", local :"jorge schimmelpfeng"},
-{ id: "relatorio2", entregador: "alex", local :"morumbi"},
-{ id: "relatorio3", entregador: "hytalo", local :"ayrton senna"},
-{ id: "relatorio4", entregador: "felipe", local :"felipe wandscheer"},
-{ id: "relatorio5", entregador: "alexandre", local :"av. parana"},
-{ id: "relatorio6", entregador: "jair", local :"av. brasil"},
-{ id: "relatorio7", entregador: "luiz", local :"rua urupes"},
-{ id: "relatorio8", entregador:  "dilma" , local :"conj. Libra"},
+const acompanhamntos=[
 
+    { id: "acompanhamento1", nomea: "batata pequena snack"},
+    { id: "acompanhamento1", nomea: "batatas do chefe"},
+    { id: "acompanhamento1", nomea: "batatas com chedar"},
+    { id: "acompanhamento1", nomea: "batata grande"},
+    { id: "acompanhamento1", nomea: "batata média"},
+    { id: "acompanhamento1", nomea: "torta de maçã"},
+    { id: "acompanhamento1", nomea: "batata rústica"},
+    { id: "acompanhamento1", nomea:  "batata com bacon" },
 
+]
+const bebidas=[
 
-
-
-
-
+    { id: "acompanhamento2", nomeb: "coca cola"},
+    { id: "acompanhamento2", nomeb: "fanta laranja"},
+    { id: "acompanhamento2", nomeb: "fanta uva"},
+    { id: "acompanhamento2", nomeb: "sprite"},
+    { id: "acompanhamento2", nomeb: "chá gelado"},
+    { id: "acompanhamento2", nomeb: "suco de laranja"},
+    { id: "acompanhamento2", nomeb: "suco de beterraba"},
+    { id: "acompanhamento2", nomeb: "água com gás" },
 
 ]
